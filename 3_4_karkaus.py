ffdf3ab4abeea08cc2ad8bb5f5d0d8229edcab1b
⚜️Ju5tu5⚜️ <108062300+Aqentjus@users.noreply.github.com>
2026-01-12 12:47:37 +0200

    Update 3_4_karkaus.py

--- 3_4_karkaus.py
+++ 3_4_karkaus.py
@@ -1,6 +1,10 @@
-vuosi = int(input("Kirjoita vuosi: "))
+vuosi = int(input("Anna vuosiluku: "))
 
-if vuosi % 4:
-    print("karkailu")
+if vuosi % 400 == 0:
+    print("Vuosi on karkausvuosi")
+elif vuosi % 100 == 0:
+    print("Vuosi ei ole karkausvuosi")
+elif vuosi % 4 == 0:
+    print("Vuosi on karkausvuosi")
 else:
-    print("nope")
+    print("Vuosi ei ole karkausvuosi")
