c7a6ebd703b6f2fc866e91f6a7eb76000d075117
⚜️Ju5tu5⚜️ <108062300+Aqentjus@users.noreply.github.com>
2026-01-12 11:11:02 +0200

    Create 2_4_lukuja.py

---
+++ 2_4_lukuja.py
@@ -0,0 +1,8 @@
+luku1 = int(input("Syöttäisitkö luvun 1: "))
+luku2 = int(input("Syöttäisitkö luvun 2: "))
+luku3 = int(input("Syöttäisitkö luvun 3: "))
+
+
+print(f'Lukujen summa on: {luku1+luku2+luku3}')
+print(f'Lukujen tulo on: {luku1*luku2*luku3}')
+print(f'Lukujen keskiarvo on: {(luku1+luku2+luku3)/int(3)}')
