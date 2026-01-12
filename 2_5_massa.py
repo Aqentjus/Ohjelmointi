65cb5dc971f78b1ea1678e675cfb760ac9108815
⚜️Ju5tu5⚜️ <108062300+Aqentjus@users.noreply.github.com>
2026-01-12 12:06:36 +0200

    Update 2_5_massa.py

--- 2_5_massa.py
+++ 2_5_massa.py
@@ -1,1 +1,16 @@
-leiviska_massa = pass
+leiviska_massa = float(8512) # 20 × 425,6 g
+naula_massa = float(425.6) # 32 × 13,3 g
+luoti_massa = float(13.3) #suoraan ilmoitettu grammoissa
+
+leiviska_maara = float(input("Anna leiviskät."))
+naulat_maara = float(input("Anna naulat."))
+luodit_maara = float(input("Anna luodit."))
+
+vastaus = float((leiviska_massa*leiviska_maara)+(naula_massa*naulat_maara)+(luodit_maara*luoti_massa))
+
+kilot = int(vastaus // 1000)
+grammat = float(vastaus % 1000)
+
+
+print(f'{kilot} kilogrammaa')
+print(f'{grammat} grammaa')
