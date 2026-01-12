9a439d20b42a25ec43f7ee24ff7c76832d777143
⚜️Ju5tu5⚜️ <108062300+Aqentjus@users.noreply.github.com>
2026-01-12 11:05:49 +0200

    Create 2_3_suorakulmio.py
    lisää tehtäviä

---
+++ 2_3_suorakulmio.py
@@ -0,0 +1,5 @@
+kanta = float(input("Antaisitko suorakulmion kannan: "))
+korkeus = float(input("Antaisitko suorakulmion korkeuden: "))
+
+print(f'Suorakulmion pinta-ala on: {kanta*korkeus}')
+print(f'Suorakulmion piiri on: {kanta*2+korkeus*2}')
