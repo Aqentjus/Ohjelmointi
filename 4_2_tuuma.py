5df70338308a7675a5064ea9051c8c2c66763842
⚜️Ju5tu5⚜️ <108062300+Aqentjus@users.noreply.github.com>
2026-01-12 13:01:36 +0200

    Update 4_2_tuuma.py

---
+++ 4_2_tuuma.py
@@ -0,0 +1,12 @@
+
+
+
+
+while True:
+    tuuma = int(input("Antaisitko pituuden tuumissa: "))
+    if tuuma < 0:
+        break
+    elif tuuma > 0:
+        print(f'{tuuma} Tuumaa on {tuuma*2.54} Senttiä')
+    else:
+        pass
