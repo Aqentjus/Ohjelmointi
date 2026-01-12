78c459f81584d10a761fd64499c849df59d13505
⚜️Ju5tu5⚜️ <108062300+Aqentjus@users.noreply.github.com>
2026-01-12 12:39:01 +0200

    Update 3_3_hemo.py

--- 3_3_hemo.py
+++ 3_3_hemo.py
@@ -7,11 +7,18 @@ mies_max = 195
 
 
 sukupuoli = str(input("Antaisitko sukupuolesi? (mies, nainen): ")).lower()
+hemoglobiini = int(input("Antaisitko hemoglobiini arvosi?: "))
 
 if sukupuoli == "nainen":
-    pass
+    if (hemoglobiini >= nainen_min) and (hemoglobiini <= nainen_max):
+        print("Hemoglobiini arvosi on normaali")
+    else:
+        print("Hemoglobiini arvosi ei ole normaalilla välillä: 117-175 g/l.")
 elif sukupuoli == "mies":
-    pass
+    if (hemoglobiini >= mies_min) and (hemoglobiini <= mies_max):
+        print("Hemoglobiini arvosi on normaali")
+    else:
+        print("Hemoglobiini arvosi ei ole normaalilla välillä: 134-195 g/l.")
 else:
     print("Virheellinen Sukupuoli")
 
