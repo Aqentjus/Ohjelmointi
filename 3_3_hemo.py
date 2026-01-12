18dcede17102e386e8acb1804a77fd5b9a248e61
⚜️Ju5tu5⚜️ <108062300+Aqentjus@users.noreply.github.com>
2026-01-12 12:31:26 +0200

    Create 3_3_hemo.py

---
+++ 3_3_hemo.py
@@ -0,0 +1,17 @@
+
+
+nainen_min = 117
+nainen_max = 175
+mies_min = 134
+mies_max = 195
+
+
+sukupuoli = str(input("Antaisitko sukupuolesi? (mies, nainen): ")).lower()
+
+if sukupuoli == "nainen":
+    pass
+elif sukupuoli == "mies":
+    pass
+else:
+    print("Virheellinen Sukupuoli")
+
