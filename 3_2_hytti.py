752455c5861f63b1a174f2d65d5b43d95ee6bcf1
⚜️Ju5tu5⚜️ <108062300+Aqentjus@users.noreply.github.com>
2026-01-12 12:25:14 +0200

    Create 3_2_hytti.py

---
+++ 3_2_hytti.py
@@ -0,0 +1,14 @@
+
+
+hytti = input("Anna hyttiluokka (LUX, A, B, C)")
+
+if hytti == "LUX":
+    pass
+elif hytti == "A":
+    pass
+elif hytti == "B":
+    pass
+elif hytti == "C":
+    pass
+else:
+    print("Virheellinen hyttiluokka")
