155fef46c3bea98c5542b1a343e6456e82cae40f
⚜️Ju5tu5⚜️ <108062300+Aqentjus@users.noreply.github.com>
2026-01-15 11:08:12 +0200

    gal_calc

--- 6_3_bensa.py
+++ 6_3_bensa.py
@@ -1,6 +1,16 @@
 
 
 
+gallona_litroissa = 3.785
 
-def bensa_litroiksi():
-    pass
+
+def bensa_litroiksi(syote):
+    litroina = syote / gallona_litroissa
+    return litroina
+
+
+while True:
+    syote = float(input("Antaisitko gallonoiden määrän?: "))
+    if syote <= 0:
+        break
+    print(bensa_litroiksi(syote))
