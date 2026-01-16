969ec4048e3c49257ab3087a100feae52cc96f36
⚜️Ju5tu5⚜️ <108062300+Aqentjus@users.noreply.github.com>
2026-01-16 13:13:41 +0200

    vuodenajat_dictionary

---
+++ 7_1_kuukausi.py
@@ -0,0 +1,17 @@
+vuodenajat = {
+    "tammikuu": "kevät",
+    "helmikuu": "kevät",
+    "maaliskuu": "kevät",
+
+    "huhtikuu": "kesä",
+    "toukokuu": "kesä",
+    "kesäkuu": "kesä",
+
+    "heinäkuu": "syksy",
+    "elokuu": "syksy",
+    "syyskuu": "syksy",
+
+    "lokakuu": "talvi",
+    "marraskuu": "talvi",
+    "joulukuu": "talvi"
+}
