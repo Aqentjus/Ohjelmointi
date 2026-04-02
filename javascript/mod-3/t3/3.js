9985df45a9bf383848b3cdf790d5c388a03162be
Aqentjus <108062300+Aqentjus@users.noreply.github.com>
2026-04-02 12:34:53 +0300

    task 3 done

--- javascript/mod-3/t3/3.js
+++ javascript/mod-3/t3/3.js
@@ -1,2 +1,13 @@
 'use strict';
+
+
 const names = ['John', 'Paul', 'Jones'];
+
+
+let ul = document.getElementById("target");
+
+names.forEach(name => {
+  let li = document.createElement("li");
+  li.textContent = name;
+  ul.appendChild(li);
+});
